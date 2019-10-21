d96fdf9a263c7e6c35d0ac3ec72ab233da477ed2
JameryLin <835127367@qq.com>
2019-10-22 00:14:16 +0800

    page finish:learnUser

--- pages/learnUser/learnUser.js
+++ pages/learnUser/learnUser.js
@@ -5,7 +5,23 @@ Page({
    * 页面的初始数据
    */
   data: {
+    username:'小草',
+    integral:'20',
 
+    recommendTitle1:'课程名称1',
+    recommendIntro1:'课程简介1',
+
+    recommendTitle2: '课程名称2',
+    recommendIntro2: '课程简介2',
+
+    recommendTitle3: '课程名称3',
+    recommendIntro3: '课程简介3',
+
+    recommendTitle4: '课程名称4',
+    recommendIntro4: '课程简介4',
+
+    recommendTitle5: '课程名称5',
+    recommendIntro5: '课程简介5',
   },
 
   /**
